a76121b721986b5044f7ddfc32eaffd9bb6b8f2c
Craig McLeod <mcleod.craig@outlook.com>
2020-06-17 14:35:22 +0100

    Test can check there no films from a particular year passed

--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -48,7 +48,11 @@ describe('Cinema', function () {
     const actual = cinema.filterByYear(films, 2017)
     assert.deepStrictEqual(actual, expected)
   });
-  // it('should be able to check whether there are no films from a particular year');
+  it('should be able to check whether there are no films from a particular year', function () {
+    const expected = []
+    const actual = cinema.filterByYear(films, 2020)
+    assert.deepStrictEqual(actual, expected)
+  });
   // it('should be able to check whether all films are over a particular length');
   // it('should be able to calculate total running time of all films');
 
